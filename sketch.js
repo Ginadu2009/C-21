4168091f4ca8680ed069e5971332247d25d5741c
Ginadu2009 <90216915+Ginadu2009@users.noreply.github.com>
2022-04-22 10:08:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var tela;
+var bloco1,bloco2,bloco3,bloco4;
+var bola, arestas;
+var music;
+
+function preload(){
+    // carregue o som aqui
+    music = loadSound("music.mp3");
+}
+
+function setup(){
+    tela = createCanvas(800,600);
+
+    bloco1 = createSprite(0,580,360,30);
+    bloco1.shapeColor = "blue";
+
+    bloco2 = createSprite(295,580,200,30);
+    bloco2.shapeColor = "orange";
+
+    bloco3 = createSprite(495,580,160,30);
+    bloco3.shapeColor = "green";
+
+    bloco3 = createSprite(795,580,60,30);
+    bloco3.shapeColor = "yellow";
+
+    bola = createSprite(random(20,750),100, 40,40);
+    bola.shapeColor = rgb(255,255,255);
+    bola.velocityX = 5
+    bola.velocityY = 5
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    arestas=createEdgeSprites();
+    bola.bounceOff(arestas);
+
+    
+    //escreva o código de ricochete de bola para bloco1 
+    if(bloco1.isTouching(bola)){
+        bola.shapeColor = "blue";
+        music.play();
+    }
+
+
+    if(bloco2.isTouching(bola)){
+        bola.shapeColor = "orange";
+        music.play();
+    }
+
+    //escreva o código de ricochete de bola para bloco3
+    if(bloco3.isTouching(bola)){
+        bola.shapeColor = "green";
+        music.play();}
+
+    //escreva o código de ricochete de bola para bloco4
+    if(bloco4.isTouching(bola)){
+        bola.shapeColor = "yellow";
+        music.play();}
+    
+    drawSprites();
+    }
